fa76b01ca1e9c61431d81171b81b1d2a94fccdea
Jose Gomez <josegomez@Joses-MacBook-Air-2024.local>
2026-09-09 14:51:22 -0700

    Added second function testing

--- test_assignment.py
+++ test_assignment.py
@@ -1,7 +1,7 @@
 import pytest
 from assignment import concert_calculator, codename_gen
 
-@pytest.mark.parametrize ( "ticket_price, num_tickets, sale_tax, processing_fee, expected", [
+@pytest.mark.parametrize( "ticket_price, num_tickets, sale_tax, processing_fee, expected", [
     (100, 3, 0.1, 5.50, "The price for 3 tickets at $100 each with 10.0% sales tax and $5.5 processing fee is $335.5"),
     (500, 3, 0.1, 5.50, "The price for 3 tickets at $500 each with 10.0% sales tax and $5.5 processing fee is $1655.5"),
     (100, 3, 0.5, 5.50, "The price for 3 tickets at $100 each with 50.0% sales tax and $5.5 processing fee is $455.5"),
@@ -14,3 +14,15 @@ from assignment import concert_calculator, codename_gen
 def test_concert_calculator(ticket_price, num_tickets, sale_tax, processing_fee, expected):
     assert concert_calculator(ticket_price, num_tickets, sale_tax, processing_fee) == expected
 
+@pytest.mark.parametrize( "first, last, fav_word, expected", [
+    ("Bruce", "Wayne", "Bat", "Mr.Bayne-3"),
+    ("diana", "Prince", "Truth", "Mr.Dinca-5"),
+    ("Clark", "Kent", "Flight", "Mr.Centk-6"),
+    ("TONY", "stark", "Jarvis", "Mr.Ttary-6"),
+    ("natasha", "romanoff", "Widow", "Mr.Nanoa-5"),
+    ("peter", "PARKER", "Web", "Mr.Prker-3"),
+    ("Arthur", "Cur", "Ocean", "Mr.Acurr-5"),
+    ("Barry", "Allen", "", "Mr.Blley-0"),
+])
+def test_codename_gen(first, last, fav_word, expected):
+    assert codename_gen(first, last, fav_word) == expected
